8743f53ae85ddf722788e734ea4cbba6c285d810
Marcos <marcosandrade.it@gmail.com>
2024-12-05 17:47:44 -0300

    WIP: Implemented HTML layout for recipes and search input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,36 @@
+import { ChangeEvent, useState } from "react"
+
+interface Recipe {
+  id: string
+  name: string
+  ingredients: string[]
+  instructions: string[]
+  imageURL?: string
+}
+
 export default function App() {
-  return <h1> Hello </h1>
+  const [recipes, setRecipes] = useState<Recipe[]>([])
+  const [search, setSearch] = useState<string>('')
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+  }
+
+  return (
+    <main>
+      <h1> Recipes </h1>
+      <input type="text" name="q" onChange={handleSearch} value={search} />
+      <div className="recipes">
+        {recipes.map((recipe: Recipe) =>
+          <div key={recipe.id}>
+            <h2>{recipe.name}</h2>
+            <img src={recipe.imageURL} alt={`A ${recipe.name} dish`} />
+            <p> Ingredients: {recipe.ingredients.join(',')}</p>
+            <ol>
+              {recipe.instructions.map((step) => <li>{step}</li>)}
+            </ol>
+          </div>
+        )}
+      </div>
+    </main>
+  )
 }
